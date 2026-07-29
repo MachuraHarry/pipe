b9495ebb9f606c933c312a40d8d39f9355142751
harrymachura6-coder <harrymachura6@gmail.com>
2026-07-30 00:38:32 +0200

    playground: code-sharing mit Firebase + SHA256-URLs

---
+++ website/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxBZ_QsQA7Rutf7dng-TWFV4-RHS0ZFwU",
+  authDomain: "pipe-a3ad1.firebaseapp.com",
+  databaseURL: "https://pipe-a3ad1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pipe-a3ad1",
+  storageBucket: "pipe-a3ad1.firebasestorage.app",
+  messagingSenderId: "470534405826",
+  appId: "1:470534405826:web:c099cd9bfb2a4f9a45855b",
+  measurementId: "G-C9PYTF4H0D"
+};
+
+firebase.initializeApp(firebaseConfig);
